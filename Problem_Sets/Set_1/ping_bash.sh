1c56c63903151ba63c02f853b65f389cce7f5697
Vidur Sarin <vidur.sarin@colorado.edu>
2017-09-17 15:28:50 -0600

    adding HW assigments as well

---
+++ Problem_Sets/Set_1/ping_bash.sh
@@ -0,0 +1,6 @@
+date
+ping -c 20 -s 300 cs.colorado.edu
+date
+ping -c 20 -s 300 uscis.gov 
+date
+ping -c 20 -s 300 amazon.in
---
+++ Problem_Sets/Set_1/trace_bash.sh
@@ -0,0 +1,4 @@
+#Print the Date and time 
+date
+#Print the traceroute output
+sudo traceroute -T -d google.com
