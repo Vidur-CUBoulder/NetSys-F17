fe52d29e1a69612da742229608e641ab69e7566c
Vidur Sarin <vidur.sarin@colorado.edu>
2017-10-04 18:10:55 -0600

    updated code; simple HTTP server working

--- Programming_Assignments/Web_Server/common.h
+++ Programming_Assignments/Web_Server/common.h
@@ -9,22 +9,53 @@
 #include<arpa/inet.h>
 #include<netdb.h>
 #include<unistd.h>
+#include<inttypes.h>
 
 #define TCP_SOCKETS SOCK_STREAM
 
-int populate_socket_addr(struct sockaddr_in *sock, uint8_t sock_family,\
-                            uint16_t sock_port, in_addr_t sock_addr)
-{
-  bzero(sock, sizeof(struct sockaddr_in)); 
-  sock->sin_family = sock_family;
-  sock->sin_port = sock_port;
-  sock->sin_addr.s_addr = sock_addr;
+//#define DEBUG_CONNECTIONS
 
-  int sock_fd = socket(sock_family, TCP_SOCKETS, 0);
-  if(sock_fd < 0) {
-    perror("ERROR:socket()");
-    exit(1);
+/* Let's create a list/array of the keywords that we are searching for */
+#if 0
+char *ws_conf_keywords[] = {   "Listen",\
+                               "DocumentRoot",\
+                               "DirectoryIndex",\
+                               "."
+                            };
+#endif
+
+typedef struct ws_config_data {
+  size_t port;
+  char directory_root[50];
+  char default_html[20];
+  char file_index[20][30];
+} config_data;
+
+void Create_Server_Connections(int *server_sock, struct sockaddr_in *server_addr,\
+                                int server_addr_len, int tcp_port)
+{
+  if(server_addr == NULL) {
+    return;
   }
-  return sock_fd;
+
+  (*server_sock) = socket(AF_INET, TCP_SOCKETS, 0);
+
+  /* set the parameters for the server */
+  server_addr->sin_family = AF_INET;
+  server_addr->sin_port = htons(tcp_port);
+  server_addr->sin_addr.s_addr = INADDR_ANY;
+
+  /* Bind the server to the client */
+  bind(*server_sock, (struct sockaddr *)server_addr, server_addr_len);
+
+  /* Start listening for the connection on the socket 
+   * Currently, limit the backlog connections to 5. This value will be 
+   * maxed out depending on the value that is in /proc/sys/ipv4/tcp_max_syn_backlog
+  */
+  listen(*server_sock, 5);
+
+  return;
 }
+
+
 
---
+++ Programming_Assignments/Web_Server/webserver.c
@@ -0,0 +1,103 @@
+#include "common.h"
+
+void WS_Parse_Config_File(config_data *ws_data)
+{
+  FILE *fp = NULL;
+  ssize_t read_line = 0;
+  char *line = (char *)malloc(50);
+  int cntr_words = 0;
+  char *eptr;
+
+  fp = fopen("./Materials/ws.conf", "r");
+  if(fp == NULL) {
+    /* Unable to open the file */
+    exit(EXIT_FAILURE);
+  }
+  
+  while( (read_line = getline(&line, &read_line, fp)) != -1) 
+  {
+    /* Ignore statements starting with '#' */
+    if(line[0] == '#')
+      continue;
+   
+    char *buffer = strtok(line, " \n"); 
+    while(buffer != NULL) 
+    {
+      switch(cntr_words)
+      {
+        case 1: /* Store the Port number */
+                ws_data->port = strtol(buffer, &eptr, 10);
+                //printf("cntr_words: %d; port: %lu\n", cntr_words, ws_data->port);
+                break;
+
+        case 3: /* Store the directory root */
+                strcpy(ws_data->directory_root,buffer); 
+                //printf("%s\n", ws_data->directory_root);
+                break;
+
+        case 5: /* Store the default index file name */
+                strcpy(ws_data->default_html, buffer);
+                //printf("%s\n", ws_data->default_html);
+                break;
+
+        default:
+                break;
+      }
+     
+      if(cntr_words >= 6) {
+        strcpy(ws_data->file_index[cntr_words-6], buffer);
+        //printf("%s\n", ws_data->file_index[cntr_words-6]);
+      }
+      
+      buffer = strtok(NULL, " \n");
+      cntr_words++;
+    }
+  }
+  
+  fclose(fp);
+  free(line);
+
+  return;
+}
+
+
+int main(int argc, char *argv[])
+{
+  /* First order of business is to open the .conf file, parse it 
+   * and store its contents in data structures. All of these will
+   * be used later.
+   */
+  config_data ws_data; 
+  WS_Parse_Config_File(&ws_data); 
+ 
+#ifdef DEBUG_CONNECTIONS
+  char http_header[2048] = "HTTP/1.1 200 OK\n\n";
+  char response_data[1024] = "Hello There!! WebBrowser";
+  strcat(http_header, response_data);
+#endif
+
+  /* Now, create the TCP connection to the client(browser) */
+  int server_sock = 0;
+  struct sockaddr_in server_addr;
+  memset(&server_sock, 0, sizeof(server_addr));
+  
+  /* Setup the webserver connections */
+  Create_Server_Connections(&server_sock, &server_addr,\
+                    sizeof(server_addr), ws_data.port);
+
+  /* Provision for client connections */
+  int client_sock = 0;
+  while(1) {
+    client_sock = accept(server_sock, NULL, NULL);
+    
+    /* Send data to the client */
+    send(client_sock, http_header, sizeof(http_header), 0); 
+
+    /* Close the client socket connection once you are done */
+    close(client_sock);
+  }
+  
+  return 0;
+}
+
+
